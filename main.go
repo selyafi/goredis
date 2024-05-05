2b39a3eaf925eab7300068eea208984f06f4bfbe
Shadi Elyafi <elyafi@gmail.com>
2024-05-06 00:08:24 +0300

    proto

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"log/slog"
 	"net"
@@ -18,6 +19,7 @@ type Server struct {
 	ln        net.Listener
 	addPeerCh chan *Peer
 	quitCh    chan struct{}
+	msgCh     chan []byte
 }
 
 func NewServer(cfg Config) *Server {
@@ -29,6 +31,7 @@ func NewServer(cfg Config) *Server {
 		peers:     make(map[*Peer]bool),
 		addPeerCh: make(chan *Peer),
 		quitCh:    make(chan struct{}),
+		msgCh:     make(chan []byte),
 	}
 }
 
@@ -43,9 +46,19 @@ func (s *Server) Start() error {
 	return s.acceptLoop()
 }
 
+func (s *Server) handleRawMessage(rawMsg []byte) error {
+	fmt.Println(string(rawMsg))
+	return nil
+}
+
 func (s *Server) loop() {
 	for {
 		select {
+		case rawMsg := <-s.msgCh:
+			if err := s.handleRawMessage(rawMsg); err != nil {
+				slog.Error("raw message errpr ", "err", err)
+			}
+			fmt.Println(rawMsg)
 		case <-s.quitCh:
 			return
 		case peer := <-s.addPeerCh:
@@ -66,9 +79,12 @@ func (s *Server) acceptLoop() error {
 }
 
 func (s *Server) handleConn(conn net.Conn) {
-	peer := NewPeer(conn)
+	peer := NewPeer(conn, s.msgCh)
 	s.addPeerCh <- peer
-	go peer.reedLoop()
+	slog.Info("new peer connected", "remoteAddr", conn.RemoteAddr())
+	if err := peer.reedLoop(); err != nil {
+		slog.Error("reed loop error", "err", err, "remoteAddr", conn.RemoteAddr())
+	}
 }
 
 func main() {
--- peer.go
+++ peer.go
@@ -1,19 +1,32 @@
 package main
 
-import "net"
+import (
+	"log/slog"
+	"net"
+)
 
 type Peer struct {
-	conn net.Conn
+	conn  net.Conn
+	msgCh chan []byte
 }
 
-func (p *Peer) reedLoop() {
+func (p *Peer) reedLoop() error {
+	buf := make([]byte, 1024)
 	for {
-
+		n, err := p.conn.Read(make([]byte, 1024))
+		if err != nil {
+			slog.Error("read error", "err", err)
+			return err
+		}
+		msgBuf := make([]byte, n)
+		copy(msgBuf, buf[:n])
+		p.msgCh <- msgBuf
 	}
 }
 
-func NewPeer(conn net.Conn) *Peer {
+func NewPeer(conn net.Conn, msgCh chan []byte) *Peer {
 	return &Peer{
-		conn: conn,
+		conn:  conn,
+		msgCh: msgCh,
 	}
 }
